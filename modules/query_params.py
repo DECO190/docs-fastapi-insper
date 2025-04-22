0218dc108ce78c9b760aa83c8db9f3717a910aab
DECO190 <deco9871@gmail.com>
2025-04-22 10:10:28 -0300

    Add query parameter handling and request body model for item management

---
+++ modules/query_params.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+# Required query parameter
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
