3506180ef5618180f54f8ad1694aedb33c4c6d73
DECO190 <deco9871@gmail.com>
2025-04-22 10:26:05 -0300

    Refactor Item model and update endpoint definitions for improved clarity and structure

--- modules/body_fields.py
+++ modules/body_fields.py
@@ -1,21 +1,49 @@
-from typing import Annotated
-
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from typing import List, Union
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(gt=0, description="The price must be greater than zero")
-    tax: float | None = None
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: List[str] = []
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
-    results = {"item_id": item_id, "item": item}
+async def update_item(item_id: int, item: Item):
+    results = { "item_id": item_id, "item": item }
     return results
+
+
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+    return weights
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+    images: list[Image] | None = None
+
+
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
